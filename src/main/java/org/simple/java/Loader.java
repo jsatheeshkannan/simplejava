01ddd748671041ac8eead4512fea3f276542854f
satheesh kannan <jsatheeshkannan@gmail.com>
2016-11-24 15:24:29 +0530

    first commit

---
+++ src/main/java/org/simple/java/Loader.java
@@ -0,0 +1,12 @@
+package org.simple.java;
+
+public class Loader {
+
+	public static void main(String[] args) {
+	  Person person = new Person();
+	  person.setFname("Simple");
+	  person.setLname("Java");
+	  System.out.println(person.toString());
+	}
+
+}
